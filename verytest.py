a23f910fc9126729065601eb02188b9ac35c8708
Aleksandr Novozhilov <alex.newlifer@gmail.com>
2018-02-10 01:42:50 +0300

    Add running tests

--- verytest.py
+++ verytest.py
@@ -1,16 +1,50 @@
+from itertools import groupby
 import argparse
 import pathlib
+import re
+import subprocess
 
-root_path = None or '.'
+root_path = '.'
+compiler = 'iverilog'
+executor = 'vvp'
 
 
 def run():
-    print("Searching in:", root_path)
+    print("Searching in root:", root_path)
 
     path = pathlib.Path(root_path)
 
-    for filename in path.rglob('*.v'):
-        print(filename)
+    def key(x): return x.parent
+    files_group = groupby(sorted([x for x in path.rglob(
+        '*.v') if not re.match('(\w|\d)+\.test\.v', x.name)], key=key), key=key)
+
+    for parent_dir, files in files_group:
+        print('Runnint tests in:', parent_dir)
+        current_dir = pathlib.Path(parent_dir)
+
+        for filename in files:
+            test_file_name = current_dir.with_name(filename.stem + '.test.v')
+
+            print('Run test for...', filename.name)
+
+            testing_dir = str(filename.parent)
+
+            objectfile_name = filename.stem
+            compile_process = subprocess.Popen(
+                [compiler,
+                 '-o{}'.format(objectfile_name),
+                 filename.name,
+                 test_file_name.name],
+                cwd=testing_dir,
+            )
+            compile_process.wait()
+
+            test_run_process = subprocess.Popen(
+                [executor,
+                 objectfile_name],
+                cwd=testing_dir,
+            )
+            test_run_process.wait()
 
 
 class SetRoot(argparse.Action):
@@ -24,7 +58,8 @@ class SetRoot(argparse.Action):
 
 
 args_parser = argparse.ArgumentParser()
-args_parser.add_argument('--root', help='Project\'s root direcory', default='.', action=SetRoot)
+args_parser.add_argument(
+    '--root', help='Project\'s root direcory', default='.', action=SetRoot)
 
 if __name__ == '__main__':
     root = args_parser.parse_args()
